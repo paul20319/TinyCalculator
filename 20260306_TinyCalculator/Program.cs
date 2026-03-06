6310d21adccae25f823acf3c2b04ab6a0293fd42
Putz Paul <P.Putz@htlvb.at>
2026-03-06 10:51:27 +0100

    Ersteinführung

---
+++ 20260306_TinyCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _20260306_TinyCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
