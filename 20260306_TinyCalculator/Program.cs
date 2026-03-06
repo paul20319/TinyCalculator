4502cb0da9f08f125cbbd49e0cccdd0f82481d5a
Putz Paul <P.Putz@htlvb.at>
2026-03-06 11:19:04 +0100

    Taschenrechner programmiert

--- 20260306_TinyCalculator/Program.cs
+++ 20260306_TinyCalculator/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Tiny calculator");
+
+            int a = 5;
+            int b = 10;
+            Console.WriteLine($"{a} + {b} = {a+b}");
         }
     }
 }
