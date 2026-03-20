020a26e0aaa21a087ae5820e35b231ccf123fe5c
Putz Paul <P.Putz@htlvb.at>
2026-03-20 08:35:18 +0100

    Restliche Operatoren hinzugefügt

--- 20260306_TinyCalculator/Program.cs
+++ 20260306_TinyCalculator/Program.cs
@@ -13,6 +13,9 @@
             int b = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine($"{a} + {b} = {a+b}");
+            Console.WriteLine($"{a} - {b} = {a-b}");
+            Console.WriteLine($"{a} / {b} = {a/b}");
+            Console.WriteLine($"{a} * {b} = {a*b}");
 
         }
     }
